abd74471f930e76eb3b4916250eae72e57a9ec3f
Anibal Fariñas <anibal.antonio1409@gmail.com>
2023-10-16 10:36:08 -0400

    refactor: ajustar logout

--- src/auth/logout/logout.service.ts
+++ src/auth/logout/logout.service.ts
@@ -2,10 +2,14 @@ import { Response } from 'express';
 
 import { Injectable } from '@nestjs/common';
 
+import { getDefaultCokieOptions } from '../cookies';
+
 @Injectable()
 export class LogoutService {
   async logout(res: Response): Promise<void> {
-    res.cookie('sigeit-cookie', '', { maxAge: 1 });
-    res.redirect('/api');
+    res.cookie('sigeit-cookie', '', {
+      ...getDefaultCokieOptions(),
+      expires: new Date(),
+    });
   }
 }
